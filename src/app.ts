f0f2585b30910f752f9bd21b6a52590c0b94058e
WOWSwap <cto@wowswap.io>
2021-05-27 18:23:58 +0300

    use env vars

--- src/app.ts
+++ src/app.ts
@@ -4,29 +4,33 @@ import { run } from "./monitor"
 import { Router__factory } from "./types";
 
 
-const [, , providerUrl, routerAddress, multicall, sleepMs] = process.argv;
-const { PRIVATE_KEY, pm_id, instances } = process.env;
+const { PRIVATE_KEY, NODE_ENDPOINT, MULTICALL_ADDRESS, ROUTER_ADDRESS, SLEEP_TIME, START_BLOCK, pm_id, instances } = process.env;
+
+if (!NODE_ENDPOINT) throw new Error("Set NODE_ENDPOINT to run liquidator bot")
+if (!MULTICALL_ADDRESS) throw new Error("Set MULTICALL_ADDRESS to run liquidator bot")
+if (!ROUTER_ADDRESS) throw new Error("Set ROUTER_ADDRESS to run liquidator bot")
+if (!START_BLOCK) throw new Error("Set START_BLOCK to run liquidator bot")
 
 const dispatchSize = instances ? parseInt(instances) : 1;
 const dispatchId = pm_id ? parseInt(pm_id) : 0;
 
-const provider = new JsonRpcProvider(providerUrl);
+const provider = new JsonRpcProvider(NODE_ENDPOINT);
 const signer = new Wallet(PRIVATE_KEY!, provider);
 
 const router = new Router__factory()
-  .attach(routerAddress)
+  .attach(ROUTER_ADDRESS)
   .connect(provider)
   .connect(signer);
 
 run({
   provider,
-  providerUrl,
+  providerUrl: NODE_ENDPOINT,
   covalentAPI: "ckey_44edb98eba8941749fba9b9b9eb",
   signer,
   router,
-  multicall,
-  sleep: parseInt(sleepMs || "1000"),
-  startBlock: 6503236,
+  multicall: MULTICALL_ADDRESS,
+  sleep: parseInt(SLEEP_TIME || "1000"),
+  startBlock: parseInt(START_BLOCK),
   dispatchId,
   dispatchSize,
 });
